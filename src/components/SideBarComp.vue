56ff764c9dd1b53d313a17c07e5c0296b25a0bd8
hugo <hnlopez89@gmail.com>
2021-05-11 13:34:05 +0200

    modified responsive blog

--- src/components/SideBarComp.vue
+++ src/components/SideBarComp.vue
@@ -1,6 +1,7 @@
 <template>
         <nav>
           <router-link to="/">HOME</router-link>
+          <router-link to="/gentes_de_martes">BLOG</router-link>
           <router-link to="/projects">MIS PROYECTOS</router-link>
           <router-link to="/functionalities">MIS FUNCIONALIDADES</router-link>
           <router-link to="/about">ABOUT</router-link>
